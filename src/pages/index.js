fd7c7345465ac517c162c8d7eda13a1a835de6d3
Sean Glynn <sean_glynn@optum.com>
2020-07-05 18:12:23 +0100

    Message

---
+++ src/pages/index.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import {graphql, useStaticQuery} from 'gatsby'
+import get from 'lodash/get'
+import {Image, Header} from 'semantic-ui-react'
+import ProductList from '../components/ProductList'
+import SEO from '../components/SEO'
+import logo from '../images/ill-short-dark.svg'
+import Layout from '../components/Layout'
+import HeaderSubHeader from 'semantic-ui-react/dist/commonjs/elements/Header/HeaderSubheader'
+
+const StoreIndex = ({location}) => {
+  const data = useStaticQuery(graphql`
+    query IndexQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+      allMoltinProduct {
+        edges {
+          node {
+            id
+            name
+            description
+            mainImageHref
+            meta {
+              display_price {
+                with_tax {
+                  amount
+                  currency
+                  formatted
+                }
+              }
+            }
+            mainImage {
+              childImageSharp {
+                sizes(maxWidth: 600) {
+                  ...GatsbyImageSharpSizes
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const siteTitle = get(data, 'site.siteMetadata.title')
+  const products = get(data, 'allMoltinProduct.edges')
+  const filterProductsWithoutImages = products.filter(v => v.node.mainImageHref)
+  return (
+    <Layout location={location}>
+      <SEO title={siteTitle} />
+      <Header
+        as="h3"
+        icon
+        textAlign="center"
+        style={{
+          marginBottom: '2em',
+        }}
+      >
+        <Header.Content
+          style={{
+            width: '60%',
+            margin: '0 auto',
+          }}
+          content={'Damhart Creations'}
+        >
+          <Image src={logo} alt="logo" />
+        </Header.Content>
+        <HeaderSubHeader
+          style={{
+            width: 'auto',
+            fontsize: '48px',
+          }}
+          content={'sdasdsd'}
+        ></HeaderSubHeader>
+      </Header>
+      <ProductList products={filterProductsWithoutImages} />
+    </Layout>
+  )
+}
+
+export default StoreIndex
